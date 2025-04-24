492dfd88f4ec0e58f23d5ed06224a9c48f288a37
community-vercel <community-vercel>
2025-04-24 10:17:50 -0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,19 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
+
+// module.exports = nextConfig
+
+module.exports = {
+  
+  devIndicators: {},
+  publicRuntimeConfig: {
+    // Available on both server and client
+    theme: "DEFAULT",
+  },
+  images: {
+    domains: ["100.64.6.105"],
+  },
+};
